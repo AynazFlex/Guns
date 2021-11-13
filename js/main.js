2d3044cca92a684e2c3cb490023ec0cea4cc9acb
AynazFlex <87998138+AynazFlex@users.noreply.github.com>
2021-11-13 22:11:15 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
-var swiper = new Swiper(".mySwiper", {
-        spaceBetween: 4,
-        slidesPerView: 4,
-        freeMode: true,
-        watchSlidesVisibility: true,
-        watchSlidesProgress: true,
-});
-var swiper2 = new Swiper(".mySwiper2", {
-        spaceBetween: 4,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        thumbs: {
-          swiper: swiper,
-        },
-});
+"use strict"
+
+menu.onclick = () => {
+  document.body.querySelector('.menu-bar').style.display = "";
+  document.body.querySelector('.main').style.display = "none";
+}
+
+closeMenu.onclick = () => {
+  document.body.querySelector('.menu-bar').style.display = "none";
+  document.body.querySelector('.main').style.display = "";
+}
